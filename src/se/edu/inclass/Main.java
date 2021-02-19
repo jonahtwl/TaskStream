53a73a21210e367b476598818f7a5b80d9d54974
Jo-Nah <jonahtham@u.nus.edu>
2021-02-19 17:15:04 +0800

    use the new methods in main

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -21,6 +21,9 @@ public class Main {
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
+        printDataWithStreams(tasksData);
+        System.out.println("Total number of deadlines (using streams): "
+                + countDeadlinesWithStreams(tasksData));
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
